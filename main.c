98e328a8628415be16c9c56902be0343da6370e5
allagoumak <101340014+allagoumak@users.noreply.github.com>
2022-03-10 11:49:23 -0500

    Update main.c

--- main.c
+++ main.c
@@ -21,7 +21,41 @@
  * Version: 1 
  * Author: XYZ 
  * *****************************************************************************/ 
-int main(){
+header(void){
+    printf("------------------------------------------------------------------------------------------------------------------\n");
+    printf("%15s %15s %15s %20s %25s ", "ROLL NUM", "DICE#1", "DICE#2", "TOTAL ROLL", "POINT MATCH\n");
+    printf("------------------------------------------------------------------------------------------------------------------\n");
+}
 
-    printf("Random Number in between 1-6 is: %d\n",rollDice());
+int firstPlay(void){
+   int firstRoll = rollDice();
+   int secondRoll = rollDice();
+
+    if((&firstRoll + &secondRoll) == 7 || (&firstRoll + &secondRoll) == 11 ) {
+        return WON;
+    } if((&firstRoll + &secondRoll) == 2 || (&firstRoll + &secondRoll) == 3  || (&firstRoll + &secondRoll) == 12 ) {
+        return LOOSE;
+    }
+}
+
+int main(){
+    char roll[6];
+    int firstRoll;
+    int secondRoll;
+    printf("ROLL THE DICE [ENTER], to QUIT enter [q]: ");
+    scanf("%s", &roll);
+    
+    firstRoll = rollDice();
+    secondRoll = rollDice();
+
+    do{
+    if (strcmp(&roll, "enter") == 0){
+       printf(firstPlay()); 
+        scanf("%s", &roll);
+    } else if (strcmp(&roll, "q") == 0) {
+        printf("Exiting..\n");
+    } else {
+        printf("Error... You must Enter either 'ENTER' or 'q\n");
+    }
+    }while(&roll != "q");
 }
